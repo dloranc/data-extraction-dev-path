39669ae18cfa77a0f07315001be87568459c96b2
Dawid Loranc <dawidloranc@gmail.com>
2021-08-26 14:30:52 +0200

    Add PDF scraper

---
+++ pdf-scraper/scraper.py
@@ -0,0 +1,33 @@
+import tabula as tb
+
+from io import StringIO
+
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfdocument import PDFDocument
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
+from pdfminer.pdfparser import PDFParser
+
+
+# extract table
+file = 'test.pdf'
+df = tb.read_pdf(
+    file,
+    pages = '22'
+)[0]
+
+print(df)
+
+# extract text
+output_string = StringIO()
+with open('test.pdf', 'rb') as in_file:
+    parser = PDFParser(in_file)
+    doc = PDFDocument(parser)
+    rsrcmgr = PDFResourceManager()
+    device = TextConverter(rsrcmgr, output_string, laparams=LAParams())
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    for page in PDFPage.create_pages(doc):
+        interpreter.process_page(page)
+
+print(output_string.getvalue())
